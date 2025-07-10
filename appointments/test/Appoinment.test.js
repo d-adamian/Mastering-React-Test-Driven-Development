7caf88d4cd8ac72cf57fc2e54b3c507eb43d7483
Dmitrii Adamian <d_adamian@novel-soft.com>
2025-07-10 15:55:19 +0300

    First passing test

---
+++ appointments/test/Appoinment.test.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {createRoot} from 'react-dom/client';
+
+import { Appointment } from '../src/Appointment';
+
+describe("Appointment", () => {
+    it("renders the customer first name", () => {
+        const customer = { firstName: "Ashley" };
+        const component = <Appointment customer={customer} />;
+        const container = document.createElement("div");
+        document.body.appendChild(container);
+
+        const root = createRoot(container);
+        React.act(() => root.render(component));
+
+        expect(document.body.textContent).toMatch("Ashley");
+    });
+});
